08f157537978dbaf436ccf469c851899f818ab97
yuto14prog <yuto14prog@gmail.com>
2022-03-27 21:49:22 +0900

    indexを修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,31 @@
-import type { NextPage } from 'next'
 import MainLayout from '../layouts'
+import Head from 'next/head';
+import Article from '../components/article';
+import styles from '../styles/Home.module.scss'
 
-const Home: NextPage = () => {
+export default function Home(props) {
   return (
     <MainLayout>
-
+      <Head>
+        <title>Simple News</title>
+      </Head>
+      <div className={styles.main}>
+        <Article title='headlines' articles={props.topArticles} />
+      </div>
     </MainLayout>
   )
 }
 
-export default Home
+export const getStaticProps = async () => {
+  const pageSize = 10
+  const topRes = await fetch(`https://newsapi.org/v2/top-headlines?country=jp&pageSize=${pageSize}&apiKey=${process.env.NEWS_API_KEY}`)
+  const topJson = await topRes.json()
+  const topArticles = topJson?.articles
+
+  return {
+    props: {
+      topArticles,
+    },
+    revalidate: 60 * 10,
+  }
+}
